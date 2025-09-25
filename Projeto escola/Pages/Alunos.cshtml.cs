c682f80160d7fddd2d903387cae2c3261feadbc8
edson <edson@EDSON>
2025-09-24 21:01:19 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto escola/Pages/Alunos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Projeto_escola.Pages
+{
+    public class AlunosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Projeto escola/Pages/Disciplinas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Projeto_escola.Pages
+{
+    public class DisciplinasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

1ae288aacbea81bd8e95b62597b4011fdbd6c526
EdsonSouza03 <esouza351@gmail.com>
2025-09-27 19:39:30 -0300

    Criaca pagina de registro

---
+++ Projeto escola/Pages/Registroprof.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Projeto_escola.Pages
+{
+    public class RegistroprofModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
